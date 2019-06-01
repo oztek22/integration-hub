61a8a766918f2d2fb7b3fc7a9d4c5f046b554b94
oztek22 <yashndave@gmail.com>
2019-06-01 17:27:04 +0530

    create new mapping profile page

--- src/views/new-mapping/index.js
+++ src/views/new-mapping/index.js
@@ -1,13 +1,86 @@
 /** @jsx jsx */
-import React from 'react';
-import { useState } from 'react';
-import { Button, Input } from 'antd';
 import { jsx } from '@emotion/core';
 import { withRouter } from 'react-router-dom';
+import LeftArrow from '../../assets/images/left-arrow.svg'
+import AggregatorIcon from '../../assets/images/aggregate-tab.svg'
+import {
+    MappingWrapper,
+    LargeContainer,
+    InnerMapping,
+    AggregatorTabs
+} from './newMappingStyle';
 
 const NewMapping = () => {
     return (
-        <div>Test me</div>
+        <MappingWrapper>
+            <LargeContainer>
+                <InnerMapping>
+                    <div class="back-header">
+                        <div className="backAction">
+                            <div className="arrow-img">
+                                <img src={LeftArrow} alt="leftarrow" />
+                            </div>
+                            <div className="back-text">Back to Mapping List</div>
+                        </div>
+                    </div>
+                    <div className="mapping-title">
+                        Create a New Mapping Profile
+                    </div>
+                    <div className="mapping-input-section">
+                        <div className="mapping-label">
+                            Name the Mapping Profile
+                        </div>
+                        <div className="mapping-input">
+                            <input type="text" placeholder="Changing the name format" />
+                        </div>
+                    </div>
+                    <AggregatorTabs>
+                        <div className="mapping-label">
+                            Transformer Settings
+                        </div>
+                        <div className="mapping-aggregator-tabs-container">
+                            <div className="tabs-title">
+
+                                <div className="title-img"><img src={AggregatorIcon} alt="aggregate" /></div>
+                                <div className="title-subtext">Aggregator Settings</div>
+
+                            </div>
+                            <div className="tabs-content">
+                                <div className="tabs-inner-content">
+                                    <div className="tabs-left-content">
+                                        <ul className="tabs-list">
+                                            <li className="tabs-item"><div className="active tabs-inner-text">General Settings</div></li>
+                                            <li className="tabs-item"><div className="tabs-inner-text">Incoming Fields</div></li>
+                                            <li className="tabs-item"><div className="tabs-inner-text">Group By</div></li>
+                                            <li className="tabs-item"><div className="tabs-inner-text">Aggregate</div></li>
+                                            <li className="tabs-item"><div className="tabs-inner-text">Advanced</div></li>
+                                        </ul>
+                                    </div>
+                                    <div class="tabs-right-content">
+                                        <div className="mapping-input-section">
+                                            <div className="mapping-label">
+                                                Name
+                                            </div>
+                                            <div className="mapping-input">
+                                                <input type="text" placeholder="Changing the name format" />
+                                            </div>
+                                        </div>
+                                        <div className="mapping-textarea-section">
+                                            <div className="mapping-label">
+                                                Description
+                                            </div>
+                                            <div className="mapping-textarea">
+                                                <textarea placeholder="Enter the Description for this Transformer" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </AggregatorTabs>
+                </InnerMapping>
+            </LargeContainer>
+        </MappingWrapper>
     )
 };
 
---
+++ src/views/new-mapping/newMappingStyle.js
@@ -0,0 +1,156 @@
+import styled from '@emotion/styled';
+import { theme } from '../../style/theme.js';
+export const MappingWrapper = styled.div`
+  padding: 72px 0;
+`;
+export const LargeContainer = styled.div`
+  max-width:1120px;
+  padding:0 20px;
+  width:100%;
+  margin:0 auto;
+`;
+export const InnerMapping = styled.div`
+    width:100%;
+    .back-header{
+        display:flex;
+        padding:35px 0 30px;
+    }
+    .backAction{
+       display:flex;
+       align-item:center;
+       cursor:pointer;
+    }
+    
+    .arrow-img{
+        flex:0 0 15px;
+    }
+    .back-text{
+        font-size:14px;
+        font-family: 'Avenir Medium';
+        color: ${theme.color.purpleishBlue};
+        padding-left:12px;
+    }
+    .mapping-title{
+        font-size:20px;
+        font-family: 'Avenir Medium';
+        color: ${theme.color.slateGrey};
+        padding:0 0 30px;
+    }
+    
+    .mapping-label{
+        fontFamily: 'Poppins,sans-serif',
+        fontWeight: 500,
+        color: #707885;
+        font-size:12px;
+        text-transform:uppercase;
+        letter-spacing:1px;
+    }
+
+    .mapping-input{
+        max-width: 330px;
+        width: 100%;
+        padding:8px 0 0;
+        input{
+            box-shadow: none;
+            border: none;
+            border-bottom: 1px solid ${theme.color.paleBlue};
+            outline: 0;
+            padding:0 0 7px;
+            font-family: 'Avenir Roman';
+            font-size:14px;
+            color:#222f42;
+            width:100%;
+        }
+    }.mapping-textarea-section{
+       padding:24px 0 0; 
+    }
+    .mapping-textarea{
+        width:100%;
+        textarea{
+            padding:16px 20px;
+            background:${theme.color.paleGrey};
+            border-radius:2px;
+            height:80px;
+            overflow:hidden;
+            width:100%;
+            box-shadow: none;
+            border: none;
+            outline:0;
+            resize:none;
+        }
+        
+    }
+    
+`;
+export const AggregatorTabs = styled.div`
+    padding:30px 0 0;
+    .mapping-aggregator-tabs-container{
+        border:1px solid ${theme.color.paleLavender};
+        border-radius: 2px;
+        box-shadow: 0 4px 9px 0 rgba(34, 47, 66, 0.16);
+        max-height:312px;
+    }
+    .tabs-title{
+        padding:18px 30px;
+        border-bottom:1px solid ${theme.color.lightBlueGrey};
+        display:flex;
+        align-items:center;
+    }
+    .title-img{
+        flex:0 0 14px;
+    }
+    .title-subtext{
+        fontFamily: 'Poppins,sans-serif',
+        color: #222f42;
+        font-size:16px;
+        padding-left:12px;
+    }
+    .tabs-content{
+        width:100%;
+    }
+    .tabs-inner-content{
+        display:flex;
+        width:100%;
+    }
+    .tabs-left-content{
+        flex:0 0 196px;
+        width:100%;
+        padding:25px 30px;
+        border-right:1px solid ${theme.color.lightBlueGrey};
+    }
+    .tabs-list{
+        padding:0;
+        margin:0;
+        list-style:none;
+    }
+    .tabs-right-content{
+        flex:1 1 auto;
+        padding:25px 30px;
+    }
+    .tabs-item{
+        padding:10px 0;
+        &:first-child{
+            padding-top:0;
+        }
+        &:last-child{
+            padding-bottom:0;
+        }
+        &:hover{
+            .tabs-inner-text{
+                color: ${theme.color.purpleishBlue};
+                font-weight: 500;
+            }
+        }
+    }
+    .tabs-inner-text{
+        fontFamily: 'Poppins,sans-serif',
+        color: ${theme.color.slateGrey};
+        font-size:14px;
+        cursor:pointer;
+        transition:0.3s all;
+        &.active{
+            color: ${theme.color.purpleishBlue};
+            font-weight: 500;  
+        }
+    }
+`;
